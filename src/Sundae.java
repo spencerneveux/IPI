b8e5f18a7eb264c24c45625730cbad942898f555
spencerneveux <spencerneveux@gmail.com>
2018-09-12 13:23:36 -0800

    Adding overloaded constructors adding toString

--- src/Sundae.java
+++ src/Sundae.java
@@ -1,14 +1,22 @@
+import sun.security.krb5.internal.crypto.Des;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sundae extends IceCream {
     private List<DessertItem> toppings;
 
-    public Sundae() {}
+    public Sundae() {
+        this.toppings = new ArrayList<>();
+    }
 
-    public Sundae(String iceCreamFlavor, int cost, ArrayList<DessertItem> toppings) {
-        super(iceCreamFlavor, cost);
-        this.toppings = new ArrayList<>(toppings);
+    public Sundae(String iceCreamFlavor) {
+        super(iceCreamFlavor);
+        this.toppings = new ArrayList<>();
+    }
+
+    public void addToppings(DessertItem item) {
+        toppings.add(item);
     }
 
     public int getCost() {
@@ -18,6 +26,23 @@ public class Sundae extends IceCream {
         }
         cost += super.getCost();
         return cost;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (DessertItem item : toppings) {
+            sb.append(item.getName() + " ");
+        }
+        sb.append("Sundae with\n" + super.getName() + " Ice Cream");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Sundae sundae0 = new Sundae("Chocolate Chip");
+        sundae0.addToppings(new Candy("Hot Fudge", .5, 100));
+        sundae0.addToppings(new Candy("m&ms", .4, 98));
+        System.out.println(sundae0);
     }
 
 }
