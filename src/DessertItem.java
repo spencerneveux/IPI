590b1918f520c70fd0661ba81be120daf7f23380
spencerneveux <spencerneveux@gmail.com>
2018-09-17 14:21:51 -0800

    Implementing Comparable interface overriding compareTo method Implementing Max method using compareTo

--- src/DessertItem.java
+++ src/DessertItem.java
@@ -1,4 +1,6 @@
-public abstract class DessertItem {
+import sun.security.krb5.internal.crypto.Des;
+
+public abstract class DessertItem implements Comparable<DessertItem> {
 
     protected String name;
 
@@ -26,5 +28,23 @@ public abstract class DessertItem {
      * @return cost of DessertItem
      */
     public abstract int getCost();
+
+    @Override
+    public int compareTo(DessertItem item) {
+        if (this.getCost() > item.getCost())
+            return 1;
+        else if (this.getCost() < item.getCost())
+            return -1;
+        else
+            return 0;
+    }
+
+    public static DessertItem max(DessertItem item1, DessertItem item2) {
+        if (item1.compareTo(item2) > 0)
+            return item1;
+        else
+            return item2;
+    }
+
 
 }
