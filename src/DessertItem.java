b0c35bcec92d3c7e649d5c92bacfc5adbd0d0a0f
Spencer Neveux <spencerneveux@gmail.com>
2018-09-18 13:16:35 -0700

    Fixing compareTo implementing generic Object

--- src/DessertItem.java
+++ src/DessertItem.java
@@ -1,6 +1,5 @@
-import sun.security.krb5.internal.crypto.Des;
 
-public abstract class DessertItem implements Comparable<DessertItem> {
+public abstract class DessertItem implements Comparable<Object> {
 
     protected String name;
 
@@ -30,13 +29,17 @@ public abstract class DessertItem implements Comparable<DessertItem> {
     public abstract int getCost();
 
     @Override
-    public int compareTo(DessertItem item) {
+    public int compareTo(Object obj) {
+        if (obj instanceof DessertItem) {
+            DessertItem item = (DessertItem)obj;
+
         if (this.getCost() > item.getCost())
             return 1;
         else if (this.getCost() < item.getCost())
             return -1;
         else
             return 0;
+        }
     }
 
     public static DessertItem max(DessertItem item1, DessertItem item2) {
